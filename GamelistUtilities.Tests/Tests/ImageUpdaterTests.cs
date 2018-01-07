b73ff3ef2aa14c2cfdff481d7123ba6e12ea16db
benlucaslaws <benlucaslaws@gmail.com>
2018-01-07 14:03:44 +1100

    Added function to populate missing image paths based on filename

---
+++ GamelistUtilities.Tests/Tests/ImageUpdaterTests.cs
@@ -0,0 +1,48 @@
+﻿using GamelistUtilities.GameDataUpdaters;
+using GamelistUtilities.XmlDataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GamelistUtilities.Tests.Tests
+{
+    public class ImageUpdaterTests
+    {
+        [Fact]
+        public void Test_ReplaceBlankImage()
+        {
+            Game game = new Game()
+            {
+                Path = "./Game.zip",
+                Image = ""
+            };
+
+            ImageUpdater imageUpdater = new ImageUpdater();
+            string imageFormat = "./images/covers/{ROM}-image.jpg";
+            imageUpdater.PopulateBlankImagePath(game, imageFormat);
+            string actualValue = game.Image;
+            string expectedValue = "./images/covers/Game-image.jpg";
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void Test_DontReplaceNonBlankImage()
+        {
+            Game game = new Game()
+            {
+                Path = "./Game.zip",
+                Image = "./gamecover.png"
+            };
+
+            ImageUpdater imageUpdater = new ImageUpdater();
+            string imageFormat = "./images/covers/{ROM}-image.jpg";
+            imageUpdater.PopulateBlankImagePath(game, imageFormat);
+            string actualValue = game.Image;
+            string expectedValue = "./gamecover.png";
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}
---
+++ GamelistUtilities/GameDataUpdaters/ImageUpdater.cs
@@ -0,0 +1,33 @@
+﻿using GamelistUtilities.XmlDataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamelistUtilities.GameDataUpdaters
+{
+    public class ImageUpdater
+    {
+        private const string PATH_FORMAT_FILENAME_PLACEHOLDER = "{ROM}";
+
+        public void PopulateBlankImagePath(Game game, string pathFormat)
+        {
+            if (string.IsNullOrWhiteSpace(game.Image))
+            {
+                string fileName = GetFileName(game);
+                string imagePath = pathFormat.Replace(PATH_FORMAT_FILENAME_PLACEHOLDER, fileName);
+                game.Image = imagePath;
+            }
+        }
+
+        private string GetFileName(Game game)
+        {
+            string filePath = game.Path;
+            int fileNameIndex = filePath.LastIndexOf("/") + 1;
+            int extensionIndex = filePath.LastIndexOf(".");
+            int fileNameLength = extensionIndex - fileNameIndex;
+            return filePath.Substring(fileNameIndex, fileNameLength);
+        }
+    }
+}
