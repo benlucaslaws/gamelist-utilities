443fa42119326dd6cf6e1b9834db644b86b4817c
benlucaslaws <benlucaslaws@gmail.com>
2018-01-08 21:02:09 +1100

    Added functions to remove arcade-history.com headers and footers

---
+++ GamelistUtilities.Tests/Tests/DescriptionUpdaterTests.cs
@@ -0,0 +1,59 @@
+﻿using GamelistUtilities.GameDataUpdaters;
+using GamelistUtilities.XmlDataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GamelistUtilities.Tests.Tests
+{
+    public class DescriptionUpdaterTests
+    {
+        [Fact]
+        public void Test_RemoveArcadeHistoryHeader_RemovesHeader()
+        {
+            Game game = new Game()
+            {
+                Description = "Game (C) 1992 Game Company\n\nDescription\n\n(C) arcade-history.com"
+            };
+
+            DescriptionUpdater descriptionUpdater = new DescriptionUpdater();
+            descriptionUpdater.RemoveArcadeHistoryHeader(game);
+            string actualValue = game.Description;
+            string expectedValue = "Description\n\n(C) arcade-history.com";
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void Test_RemoveArcadeHistoryHeader_DoesNotRemoveHeaderLikeTextInBody()
+        {
+            Game game = new Game()
+            {
+                Description = "Game (C) 1992 Game Company\n\nDescription. Game (C) 1992 Game Company\n\n(C) arcade-history.com"
+            };
+
+            DescriptionUpdater descriptionUpdater = new DescriptionUpdater();
+            descriptionUpdater.RemoveArcadeHistoryHeader(game);
+            string actualValue = game.Description;
+            string expectedValue = "Description. Game (C) 1992 Game Company\n\n(C) arcade-history.com";
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void Test_RemoveArcadeHistoryFooter_RemovesFooter()
+        {
+            Game game = new Game()
+            {
+                Description = "Game (C) 1992 Game Company\n\nDescription\n\n(C) arcade-history.com"
+            };
+
+            DescriptionUpdater descriptionUpdater = new DescriptionUpdater();
+            descriptionUpdater.RemoveArcadeHistoryFooter(game);
+            string actualValue = game.Description;
+            string expectedValue = "Game (C) 1992 Game Company\n\nDescription";
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}
---
+++ GamelistUtilities/GameDataUpdaters/DescriptionUpdater.cs
@@ -0,0 +1,35 @@
+﻿using GamelistUtilities.XmlDataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GamelistUtilities.GameDataUpdaters
+{
+    public class DescriptionUpdater
+    {
+        private const string ARCADE_HISTORY_HEADER_REGEX = "^.*(C).*";
+        private const string ARCADE_HISTORY_FOOTER_REGEX = "\\(C\\) arcade-history.com$";
+
+
+        public void RemoveArcadeHistoryHeader(Game game)
+        {
+            Regex headerRegex = new Regex(ARCADE_HISTORY_HEADER_REGEX);
+            string formattedDescription = game.Description.TrimStart();
+            formattedDescription = headerRegex.Replace(game.Description, string.Empty);
+            formattedDescription = formattedDescription.TrimStart();
+            game.Description = formattedDescription;
+        }
+
+        public void RemoveArcadeHistoryFooter(Game game)
+        {
+            Regex footerRegex = new Regex(ARCADE_HISTORY_FOOTER_REGEX);
+            string formattedDescription = game.Description.TrimEnd();
+            formattedDescription = footerRegex.Replace(game.Description, string.Empty);
+            formattedDescription = formattedDescription.TrimEnd();
+            game.Description = formattedDescription;
+        }
+    }
+}
